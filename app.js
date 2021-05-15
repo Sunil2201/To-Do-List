6629067205093958cfade222fb1c76f42cbb522c
Sunil Ballani <72138289+Sunil2201@users.noreply.github.com>
2021-05-15 23:09:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,5 +59,29 @@ function deleteCheck(e){
 }
 
 function filterTodo(e) {
-alert("A button clicked");
+const todos = todoList.childNodes;
+todos.forEach((todo)=>{
+    if(todo.classList!==undefined){
+   switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break; 
+            case "completed":
+            if(todo.classList.contains("completed")){
+              todo.style.display="flex";
+            }else{
+                todo.style.display = "none";
+            }
+            break; 
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display="flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break; 
+                
+}
+}
+});
 }
